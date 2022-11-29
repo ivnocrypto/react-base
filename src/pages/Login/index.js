2de0147cffe2fd6ae6496c69babf918072aa74e1
ivnogg <ivnocrypto@gmail.com>
2022-11-29 14:01:35 -0300

    react_toastify

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Container } from "../../styles/GlobalStyles";
-import { Paragrafo, Title } from "./styled";
+import { Title, Paragrafo } from "./styled";
 
 export default function Login() {
   return (
