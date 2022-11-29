066a76a582c4567ac70094ea7d868e73cc1013a0
ivnogg <ivnocrypto@gmail.com>
2022-11-28 21:55:37 -0300

    init

---
+++ src/pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Container } from "../../styles/GlobalStyles";
+import { Paragrafo, Title } from "./styled";
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>
+        Login
+        <small>Oie</small>
+      </Title>
+      <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
+    </Container>
+  );
+}
