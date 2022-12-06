b84680c52866c798e1ef7613aa931d59fde8696c
ivnogg <ivnocrypto@gmail.com>
2022-12-06 19:34:56 -0300

    add_redux_saga

---
+++ src/store/modules/example/reducer.js
@@ -0,0 +1,30 @@
+import * as types from "../types";
+
+const initialState = {
+  botaoClicado: false,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case types.BOTAO_CLICADO_SUCCESS: {
+      console.log("Sucesso 🚀");
+      const newState = { ...state };
+      newState.botaoClicado = !newState.botaoClicado;
+      return newState;
+    }
+
+    case types.BOTAO_CLICADO_FAILURE: {
+      console.log("Deu erro 😡");
+      return state;
+    }
+
+    case types.BOTAO_CLICADO_REQUEST: {
+      console.log("estou fazendo a requisicao");
+      return state;
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
